b92a4f050afc6fea9d7278b1fada2e977564487e
pbasnagetr <philip@treering.com>
2019-09-08 15:18:30 -0400

    top-level view basics complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 import {BoardComponent} from "./board/board.component";
 import {BoardModel} from "./models/board.model";
 
@@ -11,8 +12,21 @@ export class AppComponent implements OnInit {
   public title = "trello-clone";
   public boards: BoardModel[] = [];
 
+  constructor(
+    public router: Router,
+  ) {
+  }
+
   public ngOnInit(): void {
-    const testBoard = new BoardModel("Main Board", []);
-    this.boards.push(testBoard);
+    for (let i = 0; i < 10; i++) {
+      const testBoard = new BoardModel("My Board " + i, []);
+      this.boards.push(testBoard);
+    }
+  }
+
+  public navToHome(): void {
+    this.router.navigateByUrl("/").catch((e) => {
+      console.error("navigate to home error", e);
+    });
   }
 }
